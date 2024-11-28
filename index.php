a55072789a02c77c53b5340eb898313f298824eb
WULAN APRIYANTI <if23.wulanapriyanti@mhs.ubpkarawang.ac.id>
2024-11-28 21:16:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ini adalah test</h1>
+    <form action="./backend/login.php" menthod="POST">
+        <input type="email" name="email" id="email" placeholder="email"/>
+        <input type="password" name="password" id="password"  placeholder="password"/>
+        <button type="sumbit">LOGIN</button>
+    </form>
+
+    
+</body>
+</html>
